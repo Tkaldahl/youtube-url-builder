4702ccfb7cbef2ad7d6a0baffc87565d24190ec1
Tkaldahl <tkaldahl90@gmail.com>
2024-01-16 23:11:47 -0500

    add playlist search result component

---
+++ src/app/playlist/components/playlist-search-result.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: 'playlist-search-result',
+    imports: [CommonModule, FormsModule],
+    standalone: true,
+    providers: [],
+    templateUrl: './playlist-search-result.component.html',
+    styleUrls: ['./playlist-search-result.component.scss']
+  })
+  export class PlaylistSearchResultComponent {
+    @Input() playlistMetadata!: PlaylistMetadata;
+  
+    constructor() {}
+  
+    getThumbnailUrl(video: PlaylistMetadata): string {
+      // TODO: implement
+      return "";
+    }
+
+  }  
+
+export interface PlaylistMetadata {
+  _id: string;
+  name: string;
+  playlist: string[];
+}
