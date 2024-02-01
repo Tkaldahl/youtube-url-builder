5899bb153ae055c8775fba4f21437eb9b9d012dd
tkaldahl <tkaldahl90@gmail.com>
2024-01-31 23:54:50 -0500

    add additional api requests

--- src/app/playlist/shared/playlist-api.service.ts
+++ src/app/playlist/shared/playlist-api.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@angular/core';
-import { PlaylistDoc, SavePlaylistRequest } from './models/playlist.models';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { DeletePlaylistRequest, SavePlaylistRequest, SearchPlaylistRequest } from "./models/playlist.models";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class PlaylistApiService {
     constructor(private http: HttpClient) {}
   
     public savePlaylist(savePlaylistReq: SavePlaylistRequest): Observable<any> {
-        const length = JSON.stringify(savePlaylistReq).length.toString();
-        return this.http.post('http://127.0.0.1:5000/save_playlist', savePlaylistReq);
+        return this.http.post("http://127.0.0.1:5000/save_playlist", savePlaylistReq, {});
+    }
+
+    public getAllPlaylists(): Observable<any> {
+        return this.http.post("http://127.0.0.1:5000/get_all_playlists", {});
+    }
+
+    public searchPlaylists(searchReq: SearchPlaylistRequest): Observable<any> {
+        return this.http.post("http://127.0.0.1:5000/search_playlists", searchReq);
+    }
+
+    public deletePlaylist(deletePlaylistReq: DeletePlaylistRequest): Observable<any> {
+        return this.http.post("http://127.0.0.1:5000/delete_playlist", deletePlaylistReq);
     }
 }
