409e54cb5b61c027ceb10607aec0f441f31b43c4
tkaldahl <tkaldahl90@gmail.com>
2024-02-01 00:09:45 -0500

    delegate http calls to PlaylistApiService

--- src/app/playlist/pages/search/playlist-search.page.ts
+++ src/app/playlist/pages/search/playlist-search.page.ts
@@ -3,70 +3,44 @@ import { Component, OnInit, effect } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { PlaylistSearchResultComponent } from "../../components/playlist-search-result/playlist-search-result.component";
 import { PlaylistDoc } from "../../shared/models/playlist.models";
+import { PlaylistApiService } from "../../shared/playlist-api.service";
 
 @Component({
-    selector: 'playlist-search',
+    selector: "playlist-search",
     imports: [CommonModule, FormsModule, PlaylistSearchResultComponent],
     standalone: true,
-    providers: [],
-    templateUrl: './playlist-search.page.html',
-    styleUrls: ['./playlist-search.page.scss']
+    providers: [PlaylistApiService],
+    templateUrl: "./playlist-search.page.html",
+    styleUrls: ["./playlist-search.page.scss"]
 })
 export class PlaylistSearchPage implements OnInit {
-    searchText: string = '';
+    searchText: string = "";
     playlists: PlaylistDoc[] = [];
 
-    constructor() {}
+    constructor(private playlistApi: PlaylistApiService) {}
 
     ngOnInit(): void {
         this.getAllPlaylists();
     }
 
     getAllPlaylists(): void {
-        fetch('http://127.0.0.1:5000/get_all_playlists', {
-            method: 'POST',
-            mode: 'cors',
-        }).then((response: Response) => {
-            return response.json();
-        }).then((playlistDocs: PlaylistDoc[]) => {
-            this.playlists = playlistDocs;
-        }).catch(error => {
-            console.error(error);
+        this.playlistApi.getAllPlaylists().subscribe((response: any) => {
+            this.playlists = response.playlists as PlaylistDoc[];
         });
     }
 
     searchPlaylists(): void {
-        const body = { search_term: this.searchText };
-
-        fetch('http://127.0.0.1:5000/search_playlists', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify(body),
-        }).then((response: Response) => {
-            return response.json();
-        }).then((playlistDocs: PlaylistDoc[]) => {
-            this.playlists = playlistDocs;
-        }).catch(error => {
-            console.error(error);
+        this.playlistApi.searchPlaylists({ search_term: this.searchText }).subscribe((response: any) => {
+            this.playlists = response.playlists as PlaylistDoc[];
         });
-
     }
 
     deletePlaylist(playlist: PlaylistDoc): void {
-        const body = { playlist_id: playlist._id };
-
-        fetch('http://127.0.0.1:5000/delete_playlist', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify(body),
-        }).then((response: Response) => {
-            return response.json();
-        }).then(() => {
+        this.playlistApi.deletePlaylist({ playlist_id: playlist._id }).subscribe(() => {
             this.playlists = this.playlists.filter((p: PlaylistDoc) => p._id !== playlist._id);
-        }).catch(error => {
-            console.error(error);
         });
     }
+
 
     loadPlaylist(playlist: PlaylistDoc): void {
         console.log(playlist);
