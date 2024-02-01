a5ee7cbce67b4681f5194bfd966edd74dbecbe31
tkaldahl <tkaldahl90@gmail.com>
2024-02-01 00:06:00 -0500

    use PlaylistApiService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { RouterModule, Routes } from '@angular/router';
-import { PlaylistCreatePage } from './playlist/pages/create/playlist-create.page';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { YoutubeService } from './youtube/services/youtube.service';
-import { CommonModule } from '@angular/common';
-import { PlaylistSearchPage } from './playlist/pages/search/playlist-search.page';
+import { RouterModule, Routes } from "@angular/router";
+import { PlaylistCreatePage } from "./playlist/pages/create/playlist-create.page";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { YoutubeService } from "./youtube/services/youtube.service";
+import { CommonModule } from "@angular/common";
+import { PlaylistSearchPage } from "./playlist/pages/search/playlist-search.page";
+import { PlaylistApiService } from "./playlist/shared/playlist-api.service";
 
 export const routes: Routes = [
-    { path: 'playlist-create', component: PlaylistCreatePage },
-    { path: 'playlist-search', component: PlaylistSearchPage },
+    { path: "playlist-create", component: PlaylistCreatePage },
+    { path: "playlist-search", component: PlaylistSearchPage },
 ];
 
 @NgModule({
@@ -22,7 +23,7 @@ export const routes: Routes = [
     exports: [
         RouterModule,
     ],
-    providers: [YoutubeService],
+    providers: [YoutubeService, PlaylistApiService],
     bootstrap: []
 })
 export class AppModule { }
