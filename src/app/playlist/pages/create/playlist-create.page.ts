c0b719cedf6d60ae9e7bffd840951aa409a9f2a8
tkaldahl <tkaldahl90@gmail.com>
2024-02-01 00:00:31 -0500

    move export action onto create page from video-list-component

--- src/app/playlist/pages/create/playlist-create.page.ts
+++ src/app/playlist/pages/create/playlist-create.page.ts
@@ -1,23 +1,26 @@
-import { Component, EventEmitter, Output, WritableSignal, signal } from '@angular/core';
-import { YTVideoMetadata, YoutubeService } from '../../../youtube/services/youtube.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { GetMetaDataRequest, YoutubePlayerComponent } from '../../../youtube/components/player/youtube-player.component';
-import { YTSearchResultComponent } from '../../../youtube/components/search-result/yt-search-result.component';
-import { VideoListComponent } from '../../../youtube/components/video-list/video-list.component';
+import { Component, EventEmitter, Output, WritableSignal, signal } from "@angular/core";
+import { YTVideoMetadata, YoutubeService } from "../../../youtube/services/youtube.service";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { GetMetaDataRequest, YoutubePlayerComponent } from "../../../youtube/components/player/youtube-player.component";
+import { YTSearchResultComponent } from "../../../youtube/components/search-result/yt-search-result.component";
+import { VideoListComponent } from "../../../youtube/components/video-list/video-list.component";
+import { ExportToParentRequest } from "../../shared/models/signal.models";
+import { PlaylistApiService } from "../../shared/playlist-api.service";
+import { SavePlaylistRequest } from "../../shared/models/playlist.models";
 
 
 @Component({
-  selector: 'app-search',
+  selector: "app-search",
   imports: [CommonModule, FormsModule, YoutubePlayerComponent, YTSearchResultComponent, VideoListComponent],
   standalone: true,
-  providers: [YoutubeService],
-  templateUrl: './playlist-create.page.html',
-  styleUrls: ['./playlist-create.page.scss']
+  providers: [YoutubeService, PlaylistApiService],
+  templateUrl: "./playlist-create.page.html",
+  styleUrls: ["./playlist-create.page.scss"]
 })
 export class PlaylistCreatePage {
-  query = '';
-  getMetaDataReq: WritableSignal<GetMetaDataRequest> = signal({requestId: "", isTransition: false});
+  query = "";
+  getMetaDataReq: WritableSignal<GetMetaDataRequest> = signal({requestId: null, isTransition: false});
   videos: any[] = [];
   videoId: WritableSignal<string> = signal("");
   searchDebouncer: NodeJS.Timeout = setTimeout(() => {}, 0);
@@ -30,9 +33,12 @@ export class PlaylistCreatePage {
     published: undefined,
     isTransition: undefined
   });
+  exportPlaylistReq: WritableSignal<ExportToParentRequest> = signal({
+    requestId: null
+  });
 
 
-  constructor(private youtubeService: YoutubeService) {}
+  constructor(private playlistApi: PlaylistApiService, private youtubeService: YoutubeService) {}
 
   public debounceSearch(event: Event) {
     if (!(event instanceof KeyboardEvent)) {
@@ -83,10 +89,22 @@ export class PlaylistCreatePage {
     this.getMetaDataReq.set({requestId, isTransition});
   }
 
+  public getCurrentPlaylistData() {
+    const requestId = this.generateUuidv4();
+    this.exportPlaylistReq.set({requestId});
+  }
+
+  public handleExportedPlaylist(exportedPlaylist: SavePlaylistRequest) {
+    // TODO: Handle the first load. This is firing on page load.
+    this.playlistApi.savePlaylist(exportedPlaylist).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
   private generateUuidv4(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
         const r = Math.random() * 16 | 0;
-        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        const v = c === "x" ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
   }
