3afcf29318cacbb50a9463055e8dcedee30e1eb2
tkaldahl <tkaldahl90@gmail.com>
2024-01-31 23:56:46 -0500

    refactor video-list component

---
+++ src/app/playlist/shared/models/signal.models.ts
@@ -0,0 +1,3 @@
+export interface ExportToParentRequest {
+    requestId: string | null;
+}
--- src/app/youtube/components/video-list/video-list.component.ts
+++ src/app/youtube/components/video-list/video-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input, Signal, effect } from '@angular/core';
-import { YTVideoMetadata, YoutubeService } from '../../services/youtube.service';
-import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, Signal, effect } from "@angular/core";
+import { YTVideoMetadata, YoutubeService } from "../../services/youtube.service";
+import { CommonModule } from "@angular/common";
+import { ExportToParentRequest } from "../../../playlist/shared/models/signal.models";
+import { SavePlaylistRequest } from "../../../playlist/shared/models/playlist.models";
 
 @Component({
-  selector: 'video-list',
+  selector: "video-list",
   imports: [CommonModule],
   standalone: true,
   providers: [YoutubeService],
-  templateUrl: './video-list.component.html',
+  templateUrl: "./video-list.component.html",
 })
 export class VideoListComponent {
   @Input() newVideo!: Signal<YTVideoMetadata>;
+  @Input() exportPlaylistReq!: Signal<ExportToParentRequest>;
+  @Output() exportedPlaylist = new EventEmitter<SavePlaylistRequest>();
   playlist: YTVideoMetadata[] = [];
   transitionVideo: YTVideoMetadata | null = null;
 
@@ -25,6 +29,14 @@ export class VideoListComponent {
         }
       }
     });
+
+    effect(() => {
+      let reqId = this.exportPlaylistReq().requestId;
+      if (reqId) {
+        this.exportPlaylist()
+      }
+  });
+
   }
 
   getThumbnailUrl(video: YTVideoMetadata): string {
@@ -49,24 +61,12 @@ export class VideoListComponent {
       transition_video_url = `https://www.youtube.com/watch?v=${this.transitionVideo.videoId}`;
     }
 
-    const process_video_req = {
+    const savePlaylistReq: SavePlaylistRequest = {
       "playlist_name": "test_playlist", // TODO: get this from the user
       "video_urls": video_urls,
       "transition_video_url": transition_video_url
     };
 
-    const length = JSON.stringify(process_video_req).length.toString();
-    fetch('http://127.0.0.1:5000/save_playlist', {
- 
-      method: 'POST',
-      mode: 'cors', // Add this line to enable CORS
-      body: JSON.stringify(process_video_req)
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    this.exportedPlaylist.emit(savePlaylistReq);
   }
 }
