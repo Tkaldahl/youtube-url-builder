6e53cb5a81bb0496c97b23969ee53b0e6675feb8
tkaldahl <tkaldahl90@gmail.com>
2024-03-17 13:10:04 -0400

    create new playlist immediately after routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterOutlet, Router } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
+import { PlaylistApiService } from "./playlist/shared/services/playlist-api.service";
 
 @Component({
   selector: "app-root",
@@ -13,9 +14,23 @@ import { HttpClientModule } from "@angular/common/http";
     RouterLinkActive,
     RouterOutlet
   ],
+  providers: [PlaylistApiService],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss"
 })
 export class AppComponent {
   title = "youtube-url-builder";
+
+  constructor(private router: Router, private playlistService: PlaylistApiService) {}
+
+  createAndForwardToNewPlaylist() {
+    this.playlistService.savePlaylist({
+      playlist_name: null,
+      playlist: [],
+      transition_video: null
+    }).subscribe(savePlaylistResponse => {
+      const newPlaylistId = savePlaylistResponse.playlist_id;
+      this.router.navigateByUrl("/playlist/" + newPlaylistId);
+    });
+  }
 }
