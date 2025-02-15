47ab90380276555a14f4cefb646aee4e2ae9847e
tkaldahl <tkaldahl90@gmail.com>
2025-02-15 07:49:37 -0500

    add PopularPlaylistsScrollerComponent import

--- src/app/playlist/pages/home/home.page.ts
+++ src/app/playlist/pages/home/home.page.ts
@@ -2,9 +2,9 @@ import { CommonModule } from "@angular/common";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { PlaylistApiService } from "../../shared/services/playlist-api.service";
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { Router, RouterOutlet } from "@angular/router";
 import { PlaylistSearchBarComponent } from "../../components/playlist-search-bar/playlist-search-bar.component";
-import { PopularPlaylistsScrollerComponent } from "../../components/popuplar-playlists-scroller/popular-playlists-scroller.component";
+import { PopularPlaylistsScrollerComponent } from "../../components/popular-playlists-scroller/popular-playlists-scroller.component";
 
 @Component({
   selector: "home",
